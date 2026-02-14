c791280e7913a560dd5c53600634b04a28097d3f
Zhyar <zhiarali595@gmail.com>
2026-02-14 09:06:15 +0300

    add tab switch functionality

--- src/index.js
+++ src/index.js
@@ -1,4 +1,41 @@
 import "./styles.css"
 import { loadMain } from "./home.js";
+import { loadAbout } from "./about.js";
+import { loadMenu } from "./menu.js";
 
 loadMain.load();
+
+const loadPage = (function (){
+    const content = document.getElementById("content");
+    const homeButton = document.getElementById("home");
+    const aboutButton = document.getElementById("about");
+    const menuButton = document.getElementById("menu");
+
+    const loadHomePage = () => {
+        homeButton.addEventListener("click", () => {
+            content.innerHTML = "";
+            loadMain.load();
+        });
+    };
+
+    const loadAboutPage = () => {
+        aboutButton.addEventListener("click", () => {
+            content.innerHTML = "";
+            loadAbout.load();
+        });
+    };
+
+    const loadMenuPage = () => {
+        menuButton.addEventListener("click", () => {
+            content.innerHTML = "";
+            loadMenu.load();
+        });
+    };
+
+    return { loadHomePage, loadAboutPage, loadMenuPage };
+
+})();
+
+loadPage.loadHomePage();
+loadPage.loadAboutPage();
+loadPage.loadMenuPage();
---
+++ src/menu.js
@@ -0,0 +1,19 @@
+import back from "../img/rest-back.jpg"
+
+const loadMenu = (function (){
+
+    const content = document.getElementById("content");
+
+    const load = () => {
+
+        const img = document.createElement("img");
+        img.src = back;
+        img.classList.add("background");
+
+        content.append(img);
+    };
+
+    return { load };
+})();
+
+export {loadMenu};
